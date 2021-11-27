824630dfbd00260f87fe35278135135402519dc9
JayTaewon <93612390+JayTaewon@users.noreply.github.com>
2021-11-27 11:28:03 -0500

    Update buttoncode.py

--- buttoncode.py
+++ buttoncode.py
@@ -1,3 +1,4 @@
+#python requests library will need to be installed on the device. use "python -m pip install requests" in a command console to install it.
 import requests
 
 # https://discord.com/api/v9/channels/914183399139336215/messages
