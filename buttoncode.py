1c1d2de45dbe13fe4f7a4703857ef4461bf7c05d
JayTaewon <93612390+JayTaewon@users.noreply.github.com>
2021-12-06 22:48:38 -0500

    Update buttoncode.py

--- buttoncode.py
+++ buttoncode.py
@@ -1,15 +1,21 @@
-import requests
+import gpiozero import Button #imports the button library so the Raspberry Pi knows what to do with the button signal
 
-token = "OTE0MTgyNjQxODcwMzMxOTA0.Ya4cRw.7ejCGxJ4iBkYxeV1yWntseCoNwc"
-channel_ds = 914183399139336215
+import requests #imports the requests library which is what allows me to send a message through discord via the RPi.
 
-def sendMessage(token, channel_id, message):   
-    url = "https://discord.com/api/v9/channels/{}/messages".format(channel_id) 
-    data = {"content": message}
-    header = {"authorization": token}
+token = "OTE0MTgyNjQxODcwMzMxOTA0.Ya4cRw.7ejCGxJ4iBkYxeV1yWntseCoNwc" #Discord token, this is what allows the message to be sent by "Me" and not just a random id.
+channel_ds = 914183399139336215 #Routing number so the function knows which discord channel to send the message to.
+
+def sendMessage(token, channel_id, message):                                      #Creating the messaging function
+    url = "https://discord.com/api/v9/channels/{}/messages".format(channel_id)    #server URL
+    data = {"content": message}                                                   #Message content
+    header = {"authorization": token}                                             #Authorizes the messages to send
     
 
-    r = requests.post(url, data=data, headers=header)
-    print(r.status_code)
+    r = requests.post(url, data=data, headers=header)                             #Actual post
+    print(r.status_code)                                                          #Only needed to see if code works without having discord on hand.
 
-sendMessage("OTE0MTgyNjQxODcwMzMxOTA0.Ya4cRw.7ejCGxJ4iBkYxeV1yWntseCoNwc", 914183399139336215, "EMERGENCY MEETING CALLED: SENDING YOU TO MEETING TABLE")
+    
+button = Button(10)                                                               #This indicates which pin on the RPi that the button is connected to
+button.wait_for_press()                                                           #This is what arms the button, and then it waits for a signal from the pressed button to execute the next line.
+
+sendMessage("OTE0MTgyNjQxODcwMzMxOTA0.Ya4cRw.7ejCGxJ4iBkYxeV1yWntseCoNwc", 914183399139336215, "EMERGENCY MEETING CALLED: SENDING YOU TO MEETING TABLE") 
